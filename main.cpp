4653a4fa3ccac122c56bb31bc3cff2a0efe8e7ba
António Azevedo <antazevedo2003@gmail.com>
2022-10-07 15:51:31 +0100

    Added example functionality for reading the .csv files

--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
 // Humble beginnings
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
 
 int main(){
+    vector<string> seperated_strings;
+    ifstream myFile;
+    myFile.open("classes.csv");
 
-    std::cout << "Hello World!" << "\n";
+    while (myFile.good()){
+        string line;
+        getline(myFile, line);
+        cout << line << "\n";
 
-    return 0;
+        stringstream ss(line);
+        string word;
+
+        while (getline(ss, word, ',')){
+            seperated_strings.push_back(word);
+        }
+        seperated_strings.push_back("\n");
+
+    }
+    for (string s: seperated_strings){
+        cout << s << " ";
+    }
+    myFile.close();
 }
