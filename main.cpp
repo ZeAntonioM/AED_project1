a19b72ace256f54fea303f94048c433ffa74313f
António Azevedo <antazevedo2003@gmail.com>
2022-10-09 01:19:44 +0100

    Added basic comments

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main(){
     ifstream myFile;
     myFile.open("classes.csv");
 
-    while (myFile.good()){
+    while (myFile.good()){ // reads the classes.csv file until there are no exceptions (including EOF), 0(l) complexity, l being the number of lines in the file
         string line;
         getline(myFile, line);
         cout << line << "\n";
@@ -23,12 +23,12 @@ int main(){
         string word;
 
         while (getline(ss, word, ',')){
-            seperated_strings.push_back(word);
+            seperated_strings.push_back(word); // seperates all words by the commas and appends them to the vector, 0(w) complexity, w being the strings in each line;
         }
         seperated_strings.push_back("\n");
 
     }
-    for (string s: seperated_strings){
+    for (string s: seperated_strings){  //prints each word on the vector, O(l*w) complexity
         cout << s << " ";
     }
     myFile.close();
