4782d73665bd6d1c27f99a5811d0735a41849d4f
Sérgio Peixoto <sergiopeixoto3@gmail.com>
2022-10-13 16:15:26 +0100

    Moved Aula constructor to cpp file

--- src/Date.cpp
+++ src/Date.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 #include "Date.h"
 
-    Date::Date(){}
-    //Date construtor
-    Date::Date(std::string day, float startingTime, float duration){
-        //Turns string into int for easier searching later maybe
-        _day = Weekdays.at(day);
+Date::Date() {}
+// Date construtor
+Date::Date(std::string day, float startingTime, float duration)
+{
+    // Turns string into int for easier searching later maybe
+    _day = Weekdays.at(day);
 
-        //Decided to keep a string version of day for quality of live improvements
-        _stringDay = day;
+    // Decided to keep a string version of day for quality of live improvements
+    _stringDay = day;
 
-        //Turns float into a better Time format
-        _startingTime.minutes = std::modf(startingTime, &_startingTime.hours);
-        _startingTime.minutes*=60;
+    // Turns float into a better Time format
+    _startingTime.minutes = std::modf(startingTime, &_startingTime.hours);
+    _startingTime.minutes *= 60;
 
-        //Calcute ending time given a duration
-        float endingTime = startingTime + duration;
-        _endingTime.minutes = std::modf(endingTime, &_endingTime.hours);
-        _endingTime.minutes*=60;
-    };
+    // Calcute ending time given a duration
+    float endingTime = startingTime + duration;
+    _endingTime.minutes = std::modf(endingTime, &_endingTime.hours);
+    _endingTime.minutes *= 60;
+};
 
 // Sets starting time
 void Date::setStartingTime(Time startingTime)
@@ -85,12 +86,14 @@ float Date::getEndingTime_f()
 }
 
 // Returns day in string
-std::string Date::getDay_s(){
+std::string Date::getDay_s()
+{
     return _stringDay;
 }
 
 // Returns day in int
-int Date::getDay_i(){
+int Date::getDay_i()
+{
     return _day;
 }
 
