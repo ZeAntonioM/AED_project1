031dcd4e722e4217b8e16d9073b14a0c21e458cb
Sérgio Peixoto <sergiopeixoto3@gmail.com>
2022-10-13 16:31:58 +0100

    Added student count to Aula

--- src/Aula.h
+++ src/Aula.h
@@ -4,20 +4,20 @@
 #include "Date.h"
 
 //Esta uma mistura de ingles com portugues i know :')
+
+//Aula por UC
 class Aula{
 private:
     std::string _classCode;
     Date _date;
+    int _studentsCount;
 
 public:
     //Default construtor
     Aula();
 
     //Aula construtor
-    Aula(std::string classCode, Date date){
-        _classCode = classCode;
-        _date = date;
-    };
+    Aula(std::string classCode, Date date, int studentCount);
 
     //Setters
     //Sets Class Code
