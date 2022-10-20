a3e5c6e6955c0d024616f9fd79a6440bf5282537
Sérgio Peixoto <sergiopeixoto3@gmail.com>
2022-10-20 17:21:22 +0100

    RAasdom shitsfasss

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@ using namespace std;
 
 //! READS THE .CSV FILE
 int main(){
+    /*
     Scraper students_classes_scraper;
     students_classes_scraper.scrape_files();
     for (auto i: students_classes_scraper.get_uc_vector()){
@@ -23,13 +24,13 @@ int main(){
             Date date = t.getClassDate();
             cout << t.getClassCode() << " no dia " << date.getDay_s() << " das " << date.getStartingTime_f() << " às " << date.getEndingTime_f() << "\n";
         }
-    }
+    }*/
    
     // Cli cli;
     // cli.startup();
 
     //Testing the new classes
-    /*
+    
     Date date1 = Date("Monday", 10.5, 1.5);
     Aula aula1 = Aula("1LEIC01", date1, 25, "T");
 
@@ -37,6 +38,6 @@ int main(){
     cout << date1.getStartingTime_f() << ' ' << date1.getEndingTime_f() << ' ' << date1.getDay_i() << endl; 
     cout << aula1.getClassCode() << ' ' << aula1.getClassDate().getDay_i() << ' ' << aula1.getStudentCount() << endl;
 
-    */
+    
     return 0;
 }
