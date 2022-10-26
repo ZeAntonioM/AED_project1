90d5cdf2be2b98a435d57537e53c38506c9f64ac
Sérgio Peixoto <sergiopeixoto3@gmail.com>
2022-10-27 00:32:59 +0100

    Made boiler plate code of what cli should be like to use and how to call functions
    not finished yet

--- src/Cli.cpp
+++ src/Cli.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-char Cli::manage_Input(vector<char> options_vector, bool allow_back) const
+char Cli::manage_Input(const vector<char> &options_vector, bool allow_back) const
 {
      char option;
 
@@ -37,92 +37,77 @@ char Cli::manage_Input(vector<char> options_vector, bool allow_back) const
      return option;
 }
 
-void Cli::startup() const
+void Cli::startup()
 {
-     char option;
-     system("clear"); // limpar o terminal ao invocar o comando da shell "clear"
-     cout << "//////////////   Schedule Tool   //////////////"
-          << "\n"
-          << "Select one of the following options:"
-          << "\n"
-          << "[1] - Search for student or student information" //student_tab()
-          << "\n"
-          << "[2] - Search for UC or UC information"   //uc_tab()
-          << "\n"
-          << "[3] - Class permute"                //class_permute_tab()
-          << "\n"
-          << "[4] - General listings"        //general_listings_tab()
-          << "\n"
-          << "[Q] - Quit tool"          
-          << "\n";
+    char option;
+    system("clear"); // limpar o terminal ao invocar o comando da shell "clear"
+    cout << "//////////////   Schedule Tool   //////////////"
+         << "\n"
+         << "Select one of the following options:"
+         << "\n"
+         << "[1] - Search for student or student information"
+         << "\n"
+         << "[2] - Search for UC or UC information"
+         << "\n"
+         << "[3] - Search for Class or Class information"
+         << "\n"
+         << "[4] - Class permute"
+         << "\n"
+         << "[Q] - Quit tool"
+         << "\n";
 
-     
-     vector<char> v = {'1','2','3','4'};
-     option = manage_Input(v, false);
+    vector<char> v = {'1','2','3','4'};
+    option = manage_Input(v, false);
 
-     switch (option)
-     {
-     case '1':
-          student_Tab();
-          break;
+    switch (option)
+    {
+    case '1':
+        student_Tab(); //TODO acabar a list_Students() e get_Student_Schedule()
+        break;
+    case '2':
+        uc_Tab();
+        break;
+    case '3':
+        class_Tab();    //TODO implement permutation functions
+        break;
+    case '4':
+        class_Permute_Tab();    //TODO everything
+        break;
+    }
 
-     case '2':
-          uc_Tab();
-          break;
-
-     case '3':
-          class_Permute_Tab();
-          break;
-     
-     case '4':
-          General_Listings_Tab();
-          break;
-     }
-     
 }
-void Cli::student_Tab() const
+void Cli::student_Tab()
 {
-     string up;
+    system("clear"); // limpar o terminal ao invocar o comando da shell "clear"
+    cout << "////////////   Students Information     ////////////"
+         << "\n"
+         << "Select one of the following options:"
+         << "\n"
+         << "[1] - Get schedule"
+         << "\n"
+         << "[2] - List Students"
+         << "\n"
+         << "[B] - Go back to main menu"
+         << "\n"
+         << "[Q] - Quit tool"
+         << "\n";
 
-     cout << "\n" << "Introduce the student's UP code (only the number):     ";
-     cin >> up;
-
-     // função para verificar se o estudante existe
-
-     // o plano é para substituir o up no printf pelo nome do estudante
-     // mas ainda não os tenho ent vai ficar o up por agr
-
-     system("clear"); // limpar o terminal ao invocar o comando da shell "clear"
-     cout << "////////////   Student up" << up <<  "     ////////////"
-          << "\n"
-          << "\n"
-          << "Select one of the following options:"
-          << "\n"
-          << "[1] - Get schedule"            //get_StudentSchedule
-          << "\n"
-          << "[2] - Get class for each UC"   //get_ClassPerUC
-          << "\n"
-          << "[B] - Go back to main menu"    
-          << "\n"
-          << "[Q] - Quit tool"
-          << "\n";
-
-     vector<char> options = {'1','2'};
-     char option = manage_Input(options, true);
+    vector<char> options = {'1','2','b','B'};
+    char option = manage_Input(options, true);
 
     switch (option)
     {
         case '1':
-            student_Tab();
+            get_Student_Schedule();
             break;
         case '2':
-            uc_Tab();
+            list_Students();
             break;
-        case '3':
-            class_Permute_Tab();
+        case 'b':
+            startup();
             break;
-        case '4':
-            general_Listings_Tab();
+        case 'B':
+            startup();
             break;
     }
 }
@@ -142,10 +127,9 @@ void Cli::uc_Tab() const{
           << "\n"
           << "Select one of the following options:"
           << "\n"
+          << "[1] - Get UC Lectures"
           << "\n"
-          << "[1] - Get UC information"
-          << "\n"
-          << "[2] - Get UC Lectures"
+          << "[2] - List UCs"
           << "\n"
           << "[B] - Go back to main menu"
           << "\n"
@@ -166,8 +150,13 @@ void Cli::general_Listings_Tab() const{
           << "\n"
           << "[2] - Student's Number"
           << "\n"
-          << "[3] - UC Vacancies"
+          << "[3] - Students in given UC"
           << "\n"
+          << "[3] - UC Occupation"
+          << "\n"
+          << "[4] - Year Occupation"
+          << "\n"
+          << "[5] - Class Occupation (Not Lecture)"
           << "[B] - Go back to main menu"
           << "\n"
           << "[Q] - Quit tool"
@@ -175,7 +164,250 @@ void Cli::general_Listings_Tab() const{
 
 }
 
-void Cli::class_Permute_Tab() const {
+void Cli::class_Permute_Tab(){
     system("clear");
 
+    cout << "////////////   Class Permute   ////////////"
+         << "\n"
+         << "Choose how to permute:"
+         << "\n"
+         << "[1] - Between two students"
+         << "\n"
+         << "[2] - One student only"
+         << "\n"
+         << "[B]- Go to previous menu"
+         << "\n"
+         << "[Q] - Quit tool"
+         << "\n";
+
+    vector<char> options = {'1','2','b','B'};
+    char option = manage_Input(options, true);
+
+    switch (option)
+    {
+        case '1':
+            permute_Between_Students();
+            break;
+        case '2':
+            permute_One_Student();
+            break;
+        case 'b':
+            student_Tab();
+            break;
+        case 'B':
+            student_Tab();
+            break;
+    }
+}
+//TODO finish this function
+void Cli::get_Student_Schedule() {
+
+    system("clear");
+
+    int up;
+    cout << "\n" << "Introduce the student's UP code (only the number): ";
+    cin >> up;
+    cout << "\n";
+
+    // função para verificar se o estudante existe
+
+    // o plano é para substituir o up no printf pelo nome do estudante
+    // mas ainda não os tenho ent vai ficar o up por agr
+
+    cout << "////////////   Student up" << up << "   ////////////"
+         << "\n"
+         << "Choose a day:"
+         << "\n"
+         << "[1] - Monday"
+         << "\n"
+         << "[2] - Tuesday"
+         << "\n"
+         << "[3] - Wednesday"
+         << "\n"
+         << "[4] - Thursday"
+         << "\n"
+         << "[5] - Friday"
+         << "\n"
+         << "[B]- Go to previous menu"
+         << "\n"
+         << "[Q] - Quit tool"
+         << "\n";
+
+    vector<char> options = {'1','2','3', '4', '5','b','B'};
+    char option = manage_Input(options, true);
+
+    switch (option)
+    {
+        case '1':
+            print_Schedule(up, 1);
+            break;
+        case '2':
+            print_Schedule(up, 2);
+            break;
+        case '3':
+            print_Schedule(up, 3);
+            break;
+        case '4':
+            print_Schedule(up, 4);
+            break;
+        case '5':
+            print_Schedule(up, 5);
+            break;
+        case 'b':
+            student_Tab();
+            break;
+        case 'B':
+            student_Tab();
+            break;
+    }
+}
+
+//TODO finish this
+void Cli::list_Students() {
+    system("clear");
+
+    cout << "////////////   List Students   ////////////"
+         << "\n"
+         << "List by:"
+         << "\n"
+         << "[1] - Alphabetical Order"
+         << "\n"
+         << "[2] - UP Number"
+         << "\n"
+         << "[3] - Class"
+         << "\n"
+         << "[4] - "
+         << "\n"
+         << "[5] - Friday"
+         << "\n"
+         << "[B]- Go to previous menu"
+         << "\n"
+         << "[Q] - Quit tool"
+         << "\n";
+
+    vector<char> options = {'1','2','3', '4', '5','b','B'};
+    char option = manage_Input(options, true);
+
+    switch (option) {
+        case '1':
+            list_Students_alpha();
+            break;
+        case '2':
+            list_Students_UP();
+            break;
+        case '3':
+
+            break;
+        case '4':
+            break;
+
+        case '5':
+
+            break;
+        case 'b':
+            student_Tab();
+            break;
+        case 'B':
+            student_Tab();
+            break;
+    }
+}
+
+void Cli::permute_Between_Students(){
+    system("clear");
+
+    int studentUp1=0, studentUp2=0;
+    cout << "////////////   Permute two Students   ////////////"
+         << "\n"
+         << "Choose the first Student (up): ";
+    cin >> studentUp1;
+
+    cout << "\nChoose the second Student (up): ";
+    cin >> studentUp2;
+    cout << "\n";
+
+    if(permute_Between_Students(studentUp1, studentUp2)){
+        cout << "Student up" << studentUp1 << " changed classes with Student up" << studentUp2 << endl;
+    }else{
+        cout << "Permute was not possible" << endl;
+    }
+}
+
+//TODO
+bool Cli::permute_Between_Students(int student1, int student2) {}
+
+void Cli::permute_One_Student(){
+    system("clear");
+
+    int studentUp1;
+    string classCode;
+    cout << "////////////   Permute one Student   ////////////"
+         << "\n"
+         << "Choose Student (up): ";
+    cin  >> studentUp1;
+    cout << "\n"
+         << "Choose Class to move student (1LEIC01 - 3LEIC15): ";
+    cin  >> classCode;
+    cout << "\n";
+
+    if(permute_One_Student(studentUp1, classCode)){
+        cout << "Student up" << studentUp1 << " was changed to class " << classCode << endl;
+    }else{
+        cout << "Permutation was not possible due to inbalance between classes";
+    }
+
+}
+
+//TODO
+bool Cli::permute_One_Student(int studentUp1, string classCodeToChangeTo) {}
+
+//TODO
+void Cli::print_Schedule(int up, int day) const {}
+
+//TODO finish this
+void Cli::list_Students_alpha(){
+    system("clear");
+
+    cout << "////////////   List Students Alphabetically   ////////////"
+         << "\n"
+         << "[1] - Ascending Order"
+         << "\n"
+         << "[2] - Descending Order"
+         << "\n";
+
+    vector<char> options = {'1','2'};
+    char option = manage_Input(options, true);
+
+    switch (option) {
+        case '1':
+            //for loop to print in ascending order
+            break;
+        case '2':
+            //for loop to print in descending order
+            break;
+    }
+}
+
+//TODO finish this
+void Cli::list_Students_UP(){
+    system("clear");
+
+    cout << "////////////   List Students By UP   ////////////"
+         << "\n"
+         << "[1] - Ascending Order"
+         << "\n"
+         << "[2] - Descending Order"
+         << "\n";
+
+    vector<char> options = {'1','2'};
+    char option = manage_Input(options, true);
+
+    switch (option) {
+        case '1':
+            //for loop to print in ascending order
+            break;
+        case '2':
+            //for loop to print in descending order
+            break;
+    }
 }
--- src/Cli.h
+++ src/Cli.h
@@ -6,11 +6,20 @@ using namespace std;
 
 class Cli{
     public:
-        void startup() const;
-        void student_Tab() const;
+        char manage_Input(const vector<char> &options_vector, bool allow_back = false /* isto é para decidir se a tab permite ir para trás*/ ) const;
+        void startup();
+        void student_Tab();
         void uc_Tab() const;
-        void class_Permute_Tab() const;
+        void class_Tab() const;
+        void class_Permute_Tab();
         void general_Listings_Tab() const;
-        char manage_Input(vector<char> options_vector, bool allow_back = false /* isto é para decidir se a tab permite ir para trás*/ ) const;
-
+        void get_Student_Schedule();
+        void list_Students();
+        void list_Students_alpha();
+        void list_Students_UP();
+        void print_Schedule(int up, int day) const;
+        void permute_Between_Students();
+        bool permute_Between_Students(int studentUp1, int studentUp2);
+        void permute_One_Student();
+        bool permute_One_Student(int studentUp1, string classCodeToChangeTo);
 };
