8a2a90fc82db8c38d7438ea5bda8c1d4ab2e76f2
ZeAntonioM <joseapmartins2003@gmail.com>
2022-10-26 18:17:07 +0100

    Doxygen documentation for Student.cpp and Aula.cpp

--- src/Aula.cpp
+++ src/Aula.cpp
@@ -1,8 +1,15 @@
 #include "Aula.h"
 #include "Date.h"
 
+/**
+ * Default construtor da classe Aula.
+*/
 Aula::Aula(){}
 
+/**
+ * Contrutor da classe Aula.
+ * É atibuida à aula o código da turma a que está associada, uma data associada à mesma e o tipo da aula. 
+*/
 Aula::Aula(std::string classCode, Date date, std::string type){
         _classCode = classCode;
         _date = date;
@@ -11,47 +18,74 @@ Aula::Aula(std::string classCode, Date date, std::string type){
 
 // Setters
 // Sets Class Code
+/**
+ * Atribui um código de turma à aula.
+*/
 void Aula::set_ClassCode(std::string classCode)
 {
     _classCode = classCode;
 }
 
 // Sets Class date
+/**
+ * Atribui uma data à aula.
+*/
 void Aula::set_ClassDate(Date date){
     _date = date;
 }
 
 //Sets class student count
+/**
+ * Atribui a quantidade de alunos incritos nessa aula.
+*/
 void Aula::set_StudentCount(int studentCount){
     _studentCount = studentCount;
 }
 
 // Sets Class Type
+/**
+ * Atribui um tipo de aula à aula em questão.
+*/
 void Aula::set_Type(std::string type){
     _type = type;
 }
 
 // Getters
 // Returns the Class Code
+/**
+ * Retorna o código da turma associada à aula.
+*/
 std::string Aula::get_ClassCode(){
     return _classCode;
 }
 
 // Returns the Class Date
+/**
+ * Retorna a data da aula.
+*/
 Date Aula::get_ClassDate(){
     return _date;
 }
 
 //Returns current student Count
+/**
+ * retorna a quantidade de alunos na turma.
+*/
 int Aula::get_StudentCount(){
     return _studentCount;
 }
 
+/**
+ * Retorna o tipo da aula em questão (T, TP, PL, etc..)
+*/
 std::string Aula::get_Type(){
     return _type;
 }
 
 //Checks equality for two Aula objects
+/**
+ * Duas aulas são iguais se a turma associada for a mesma e se ambas ocorrerem ao mesmo tempo.
+*/
 bool Aula::operator==(Aula aula) const{
     return (this->_classCode == aula._classCode && this->_type == aula._type);
 }
--- src/Student.cpp
+++ src/Student.cpp
@@ -1,27 +1,51 @@
 #include "Student.h"
 
+    /**
+     * Contrutor da Classe Student: atribui o nome  e o up do estudante. Em caso de ser criado um estudante sem atributos, estes ficam a NULL.
+    */
     Student::Student(string name_ = NULL, string up_ = NULL){
         name = name_;
         up = up_;
     }
 
-    Student::Student(){};
-
+    /**
+     * Destructor da Classe Student.
+    */
     Student::~Student(){};
 
+    /**
+     * Atibui name_ ao nome do estudante.
+    */
     void Student::set_Name(string name_) { name = name_;}
 
+    /**
+     * Atibui up_ ao número mecânografico(up) do estudante.
+    */
     void Student::set_Up(string up_) { up = up_;}
 
+    /**
+     * Retorna o nome do estudante.
+    */
     string Student::get_Name() { return name;}
 
+    /**
+     * Retorna o Up do estudante.
+    */
     string Student::get_Up() { return up;}  
 
+    /**
+     * Adiciona uma turma ao horário do estudante. 
+     * É adicionado um tuplo que contém tanto a UC como a turma a que o estudante está a ser atribuido.
+    */
     void Student::add_UcClass(Uc cadeira, Aula turma){
         tuple<Uc,Aula> novaucturma (cadeira, turma);
         schedule.push_back(novaucturma);
     }
 
+    /**
+     * Remove uma turma do estudante.
+     * Percorre as turmas em que o estudante está inscrito e remove a turma se esta for encontrada.
+    */
     void Student::remove_UcClass(Uc cadeira, Aula turma){
         tuple<Uc, Aula> UCTurmRemove (cadeira, turma);
         auto posi = schedule.begin();
